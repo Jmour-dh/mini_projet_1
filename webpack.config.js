ffa398d0987d48ba3dca21bd6cca5926f39149c5
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-07-07 09:54:49 +0200

    finition

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,8 @@ module.exports = {
     mainStyles: path.join(__dirname, "src/index.scss"),
     jouer: path.join(__dirname, "src/jouer/jouer.js"),
     jouerStyles: path.join(__dirname, "src/jouer/jouer.scss"),
+    snake: path.join(__dirname, "src/snake/snake.js"),
+    snakeStyles: path.join(__dirname, "src/snake/snake.scss"),
     form: path.join(__dirname, "src/form/form.js"),
     formStyles: path.join(__dirname, "src/form/form.scss"),
     topbar: path.join(__dirname, "src/assets/javascripts/topbar.js"),
@@ -53,6 +55,11 @@ module.exports = {
       filename: "jouer.html",
       template: path.join(__dirname, "./src/jouer/jouer.html"),
       chunks: ["jouer", "topbar", "jouerStyles"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "snake.html",
+      template: path.join(__dirname, "./src/snake/snake.html"),
+      chunks: ["snake", "topbar", "snakeStyles"],
     }),
     new HtmlWebpackPlugin({
       filename: "form.html",
